d39b7c82a667e8153a33cb4b59c1185a6f5afd4e
karthik sunkari <karthiksunkari820@gmail.com>
2024-07-15 02:07:32 +0530

    changes

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+   <section>
+       <h1 className='text-20 font-bold text-white-1'>RightSidebar</h1>
+      
+
+   </section>
+  )
+}
+
+export default RightSidebar
